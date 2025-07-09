0914084333862f9c0b02f81c6f7744b9e904be12
Leoemanuel21 <leoemanuel14.rocha@gmail.com>
2025-07-09 10:15:09 -0300

    ex

---
+++ Exercícios/7 - Condições/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60;
+console.log(`A velocidade do seu carro é ${vel}Km/h`);
+if(vel > 60.5){
+    console.log(`Você utrapassou a velocidade permitida. MULTADO`);
+}else{
+    console.log(`Dirija sempre usando ciinto de segurança!`);
+}
