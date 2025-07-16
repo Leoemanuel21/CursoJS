bd583c1587b60adf530545191cea666241726375
Leoemanuel21 <leoemanuel14.rocha@gmail.com>
2025-07-16 11:26:17 -0300

    ex

--- Exercícios/Exercício - Repetições/script.js
+++ Exercícios/Exercício - Repetições/script.js
@@ -3,15 +3,29 @@ function verificar(){
     var fim = window.document.getElementById('txtfim');
     var passo = window.document.getElementById('txtpasso');
     var res = window.document.getElementById('res');
-    var i = Number(inicio.value);
-    var f = Number(fim.value);
-    var p = Number(passo.value);
-    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 || Number(inicio.value) < 1){
+    
+    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
         res.innerHTML = 'Impossível contar!';
     }else{
-        res.innerHTML = 'Contando:';
-        for(i; i<=f; i+=p){
-        res.innerHTML += ` ${i}`;
+        res.innerHTML = 'Contando: <br>';
+        var i = Number(inicio.value);
+        var f = Number(fim.value);
+        var p = Number(passo.value);
+        if(p <= 0){
+            window.alert('Passo inválido Considerando PASSO 1')
+            p = 1;
         }
+        if(inicio < fim){
+            // Contagem Crescente
+            for(i; i<=f; i+=p){
+            res.innerHTML += `${i} \u{1f449}`;
+            }
+        }else{
+            // Contagem Regressiva
+            for(i; i>=f; i-=p){
+            res.innerHTML += `${i} \u{1f449}`;
+            }
+        }
+        res.innerHTML += ` \u{1f3c1}`;
     }
 }
